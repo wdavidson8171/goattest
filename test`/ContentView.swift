bd6c675a47f34fbf957fd64ef700512dc2b34063
mveka6979 <123995312+mveka6979@users.noreply.github.com>
2024-09-30 11:24:31 -0700

    Update ContentView.swift

--- test`/ContentView.swift
+++ test`/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     var body: some View {
         VStack{
 
-            Text("github is evil")
+            Text("github is evil yes it is")
 
             Image("goat").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
         }
