14023eca6610c1dfe0ad4c430bea362d82c7f876
Maren V <mveka6979@student.pps.net>
2024-10-04 11:32:38 -0700

    text input box

--- test`/ContentView.swift
+++ test`/ContentView.swift
@@ -11,11 +11,16 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
-
+    @State private var textInput = ""
+    
+    
     var body: some View {
         VStack{
             Text("i have MASSIVE beef with github!@#$%^&*()")
             Image("goat").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/).cornerRadius(300)
+            TextField("Name", text: $textInput)
+                .textFieldStyle(.roundedBorder)
+                .padding()
         }
         
         NavigationSplitView {
