d18814bb014404144b206982a9ed8c6c65f7fc79
Maren V <mveka6979@student.pps.net>
2024-10-08 10:37:54 -0700

    uh oh

---
+++ test`/MaybeANewPage.swift
@@ -0,0 +1,24 @@
+//
+//  MaybeANewPage.swift
+//  test`
+//
+//  Created by Maren Veka on 10/8/24.
+//
+
+import SwiftUI
+
+struct MaybeANewPage: View {
+    var body: some View {
+        Text("New Page??")
+    }
+}
+
+struct MaybeANewPage_Previews: PreviewProvider{
+    static var previews: some View {
+        MaybeANewPage()
+    }
+}
+
+#Preview {
+    MaybeANewPage()
+}
