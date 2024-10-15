756429fcd062c496efcb9fc33356d9024f0f987e
Waverly <wdavidson8171@student.pps.net>
2024-10-15 10:51:58 -0700

    messing around withcheckboxes

--- test`/Survey.swift
+++ test`/Survey.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct Survey: View {
+    @State private var didTap:Bool = false
+    @State public var family:Bool = false
     var body: some View {
     
             NavigationStack{
                 VStack{
                     Text("SURVEEEYYY")
                     NavigationLink("Done w/ survey", destination: Bingo())
+                    
+                    Button(action: {
+                        self.didTap.toggle()
+                        self.family.toggle()
+                    }) {
+                        if(self.didTap == true){
+                            Image(systemName: "circle.fill")
+                            
+                        }
+                        else{
+                            Image(systemName: "circle")
+                        }
+                        Text("Family Activites")
+                          }
                 }
             }
     }
