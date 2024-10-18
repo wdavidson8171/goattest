5f61343ce07ee74bacaa5fb5ffc7c4d06b07447a
Waverly <wdavidson8171@student.pps.net>
2024-10-18 10:18:09 -0700

    added button

--- test`/Bingo.swift
+++ test`/Bingo.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct Bingo: View {
     var body: some View {
+        VStack{
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
         NavigationStack{
             TabView{
                 NavigationLink("Bingo", destination: Bingo())
