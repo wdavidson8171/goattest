6c0235d45e7509804ee3a8fe14cb9788cfa124c7
Waverly <wdavidson8171@student.pps.net>
2024-10-24 10:50:11 -0700

    NAVIGATION FIXED

--- test`/Bingo.swift
+++ test`/Bingo.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 struct Bingo: View {
     var body: some View {
-        Spacer()
-        Spacer()
         VStack{
             HStack{
                 Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
@@ -40,7 +38,6 @@ struct Bingo: View {
                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                 }.padding()
             }
-            Spacer()
             HStack{
                 Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
