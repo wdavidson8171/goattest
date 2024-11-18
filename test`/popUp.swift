12db22540b346ecc00fe0e4eb84a39500c35c4b6
sephj <sjarosh5910@student.pps.net>
2024-11-18 11:40:21 -0800

    popup in profile!

--- test`/popUp.swift
+++ test`/popUp.swift
@@ -11,37 +11,18 @@ import SwiftUI
 
 
 struct popup: View {
-    @State var selectedItems = [String]()
-    @State var allItems:[String] = [
-        "Family activities",
-        "Group Activities",
-        "Household Chores",
-        "Indoor Activities",
-        "Creative Activities",
-        "Outdoor Activities",
-        "Exercise",
-        "Going out (cheap)",
-        "Going Out (fancy)",
-        "Cooking/Baking",
-        "Self Care",
-        "Spring",
-        "Summer",
-        "Fall",
-        "Winter"    ]
+  
     @State var showPopup = false
     func buttonPresed(){
         showPopup = true
         buttonPressed = true
             
     }
-    
-    func test(){
-        print("aghhhhh")
-    }
+
     
     var body: some View {
         Button(action: {buttonPresed()}){
-             Text("Survey")}
+            Text("Survey")}
         .popover(isPresented: $showPopup, arrowEdge: .bottom) {
             TabView {
                 Survey().tabItem{
