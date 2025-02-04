d6d4ab124fe0b094012e9fc39389e887d5db1d1c
Maren V <mveka6979@student.pps.net>
2025-02-04 11:40:14 -0800

    testing user storage with arrays except it doesn't work right now

--- test`/homepage.swift
+++ test`/homepage.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 var selectedImage = ImageResource .nada
-var ownedList: [ImageResource] = [.nada]
 var clickedImage = ImageResource .nada
 var costOfItem: Int = 0
 var tempCoins: Int = 500
+var ownedList: [ImageResource] = [.nada]
 
 
 struct Homepage: View {
     //@AppStorage("ownedList") var ownedList: [ImageResource] = [.nada]
     
+    
+    let userDefaults = UserDefaults.standard
+    UserDefaults.standard.set(ownedList, forKey: "ownedList")
+    let savedOwnedList = userDefaults.array(forKey: "ownedList") as? [ImageResource]
+    
+    /* For next class: figure out how to save an array probably use userdefaults
+     should the 1st U in "user defaults" be capitalized or not?
+     should you declare owned list first as equal to the data stored?
+     or declare it with just nada?
+     */
     
     @State var showPopup = false
     func buttonPresed(){
