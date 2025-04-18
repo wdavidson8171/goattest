05aff6990e92abba7794606e3796ef76da321a3b
Waverly <wdavidson8171@student.pps.net>
2025-04-18 11:41:16 -0700

    what

--- test`/ContentView.swift
+++ test`/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 import SwiftData
 import UIKit
+import AVFoundation
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     @State private var textInput = ""
     
+    @State var audioPlayer: AVAudioPlayer?
+    
+    func playBackgroundMusic(){
+        guard let url = Bundle.main.url(forResource: "/Users/wdavidson8171/Downloads/filled-with-sunlight-background-dramatic-music-for-video-38-second-327744.mp3", withExtension: "mp3") else { return }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+            audioPlayer?.numberOfLoops = -1
+        } catch {
+            print("Error playing music: \(error)")
+        }
+    }
+    
+    init(){
+        playBackgroundMusic()
+    }
     
     var body: some View {
         
@@ -29,10 +47,13 @@ struct ContentView: View {
             }
         }
         
+        
+        
     
     }
         
         
         
 }
+
 
